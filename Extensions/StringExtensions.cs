3761d30e81f98f81811a219426c361c6ca8716be
fpilz82 <florian.pilz@posteo.de>
2020-10-26 10:14:39 +0100

    Add string extensions

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace TestPlanTools.Extensions
@@ -21,5 +24,17 @@ namespace TestPlanTools.Extensions
 
         public static string ToUnsecureString(this SecureString secureString) =>
             Marshal.PtrToStringUni(Marshal.SecureStringToGlobalAllocUnicode(secureString));
+
+        public static string ToJson<T>(this T convert) =>
+            JsonConvert.SerializeObject(convert, Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+
+        public static string AddRandomString(this string text, int length, string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        {
+            var random = new Random();
+            return text + new string(Enumerable.Range(1, length).Select(_ => chars[random.Next(chars.Length)]).ToArray());
+        }
     }
 }
